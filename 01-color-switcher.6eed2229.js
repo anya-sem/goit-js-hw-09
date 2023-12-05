2df3aaf04417c2395b33adff9d10c0e52892ff8f
anya-sem <139631738+anya-sem@users.noreply.github.com>
2023-12-05 13:29:21 +0000

    Deploying to gh-pages from @ anya-sem/goit-js-hw-09@28384ccc51ae686c74ef3e08bea9847c469e4b5c 🚀

---
+++ 01-color-switcher.6eed2229.js
@@ -0,0 +1,2 @@
+!function(){var t,e=document.querySelector("[data-start]"),d=document.querySelector("[data-stop]");document.querySelector("body");e.addEventListener("click",(function(){e.disabled=!0,d.disabled=!1,t=setInterval((function(){document.body.style.backgroundColor="#".concat(Math.floor(16777215*Math.random()).toString(16).padStart(6,"0"))}),1e3)})),d.addEventListener("click",(function(){e.disabled=!1,d.disabled=!0,clearInterval(t)}))}();
+//# sourceMappingURL=01-color-switcher.6eed2229.js.map
